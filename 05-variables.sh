69ab8425779196acc4361c4add5155cc897a0c98
Aparna <chamirthiaparna.com>
2023-08-18 16:25:13 +0530

    added s

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,12 +2,12 @@
 
 echo "please enter your username"
 
-read USERNAME # this is the variable where the value of the one user entered
+read -s USERNAME # this is the variable where the value of the one user entered
 
 echo "user name entered is: $USERNAME"
 
 echo "please enter your password"
 
-read PASSWORD
+read -s PASSWORD
 
 echo "password entered is: $PASSWORD"
