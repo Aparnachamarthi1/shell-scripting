3b989e695ce3cf436440e8f1a62df683350c6094
Aparna <chamirthiaparna.com>
2023-08-19 17:27:23 +0530

    install mysql

--- 08-install-mysql.sh
+++ 08-install-mysql.sh
@@ -10,4 +10,12 @@ then
     exit 1
 fi
 
+# it is our responsibility again to check installation is success or not
 yum install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "installation of mysql is error"
+else
+    echo "installation of mysql is success"
+fi    
