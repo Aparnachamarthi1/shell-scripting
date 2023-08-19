7e2f19328b6545f3b9e8c3fb8d8e14eeaca1f240
Aparna <chamirthiaparna.com>
2023-08-19 11:59:23 +0530

    added less condition

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# check if the number is greater than 10 or not
+# check if the number is less than 10 or not
 NUMBER=$1
 
-if [ $NUMBER -ge 10 ]
+if [ $NUMBER -lt 10 ]
 then
-    echo "$NUMBER is greater than 10"
+    echo "$NUMBER is less than 10"
 else
-    echo "$NUMBER is not greater than 10"
+    echo "$NUMBER is not less than 10"
 fi      
